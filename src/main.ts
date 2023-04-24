0ec5cd7484df7922fadd1a7f47393d7d9b98cc69
xPotter777 <senkivsasa@gmail.com>
2023-04-25 00:40:06 +0300

    Create users resourse, add swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: false });
+
+  app.enableCors({ credentials: true, origin: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Cloud Storage App')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger-docs', app, document);
+
+  await app.listen(7777);
 }
 bootstrap();
