ec55ab5a89ff2c1f99a08165268426b5b6767d8e
xPotter777 <senkivsasa@gmail.com>
2023-04-25 00:50:14 +0300

    Connect postgres and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { FilesModule } from './files/files.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, FilesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'lallah.db.elephantsql.com',
+      port: 5432,
+      username: 'mxsfoftt',
+      password: 'h4rItN5e2m_Ik9zsXMFJVY4h8wMKL2vo',
+      database: 'mxsfoftt',
+      entities: [],
+      synchronize: true,
+    }),
+    UsersModule,
+    FilesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
